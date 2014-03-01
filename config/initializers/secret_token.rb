3df676fbd4bf4f9a59501b3eafc68bf6be2b78cc
Ptotem Learning Projects <info@ptotem.com>
2014-03-01 07:36:27 +0530

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Lhc::Application.config.secret_key_base = '01380fac30139b0255b954edf395bf710aae8dfa68cdbb4a89eae618caf91073f3e9c84549e2e9c602bc26fb9128d7001d5177a7261380d28402e1737e0ed5ea'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Lhc::Application.config.session_store :cookie_store, key: '_lhc_session'
