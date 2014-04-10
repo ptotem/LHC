1a487bb7c54d7341ca28bb88ce80b79d63032bcf
Nilesh Panchal <nilesh@ptotem.com>
2014-04-10 22:32:55 +0530

    nilesh changes

--- app/controllers/demographics_controller.rb
+++ app/controllers/demographics_controller.rb
@@ -90,9 +90,10 @@ class DemographicsController < ApplicationController
     #https://github.com/ahmetabdi/themoviedb
 
     #@movie_search_text = "Agneepath"
-    @movie_search_text = params[:movie_search_text][0]
+    @movie_search_text = params['movie_search_text'][0]
+    #@movie_search_text = params[:movie_search_text]
     #render :text => @movie_search_text
-
+    #return
     Tmdb::Api.key("a69e2d8b3e5942d8850c9d17e2dbc126")
     Tmdb::Api.language("en")
 
