f02473201573e772db754d81cfb129a56660e4dd
SauronTheGreat <contactme@rushabhhathi.com>
2014-04-13 12:27:10 +0530

    converted my dashborad from bootstrap to normal css

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+  def get_content_for(key)
+    if SiteContent.find_by_key(key).nil?
+      "Content Unavailable"
+    else
+      raw(SiteContent.find_by_key(key).value)
+
+    end
+  end
+
 end
