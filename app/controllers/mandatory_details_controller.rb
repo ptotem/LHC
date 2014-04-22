c76be6dc8a25dffcc71da84d56cde4f40351cd15
Sunny <sunny.220990@gmail.com>
2014-04-22 10:19:03 +0530

    back redirection working

--- app/controllers/mandatory_details_controller.rb
+++ app/controllers/mandatory_details_controller.rb
@@ -5,28 +5,32 @@ class MandatoryDetailsController < ApplicationController
   def fill_about_me
     @user = current_user
     @next_page  = fill_ilike_path
-
+    @prev_page = fill_drinking_path
   end
 
   def fill_ilike
     @user = current_user
     @next_page  = fill_future_plan_path
+    @prev_page = fill_about_me_path
 
   end
 
   def fill_profilepic
     @user = current_user
     @next_page = my_dashboard_path
+    @prev_page = fill_friend_speak_path
 
   end
 
   def future_plan
     @user = current_user
     @next_page  = fill_friend_speak_path
+    @prev_page = fill_ilike_path
   end
   def friend_speak
     @user = current_user
     @next_page = fill_profilepic_path
+    @prev_page = fill_future_plan_path
 
 
   end
@@ -34,11 +38,13 @@ class MandatoryDetailsController < ApplicationController
   def fill_dates
     @user = current_user
     @next_page  = fill_smoking_path
+
   end
 
   def fill_smoking
     @user = current_user
     @next_page  = fill_drinking_path
+    @prev_page = fill_dates_path
 
   end
 
@@ -46,7 +52,7 @@ class MandatoryDetailsController < ApplicationController
 
     @user = current_user
     @next_page  = fill_about_me_path
-
+    @prev_page = fill_smoking_url
   end
 
 end
