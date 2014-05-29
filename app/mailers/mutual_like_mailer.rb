afbae146c6b86220b31439be982204718e583451
Nilesh Panchal <nilesh@ptotem.com>
2014-05-29 19:34:21 +0530

    email

--- app/mailers/mutual_like_mailer.rb
+++ app/mailers/mutual_like_mailer.rb
@@ -1,8 +1,8 @@
 class MutualLikeMailer < ActionMailer::Base
   def mutual_like_mailer(user,current_user)
-    @user = User.find(userid).email
+    @user = User.find(user).email
     @current_user=current_user
-    @gender = User.find(userid).demographic.male ? "Guy" : "Gal"
+    @gender = User.find(user).demographic.male ? "Guy" : "Gal"
     mail(to: @user, subject: 'Can we do a virtual high-five? Coz you have a mutual like!!')
   end
 end
