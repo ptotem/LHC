0fde0d3289ca719531f961fc0d9f03d373cd4324
Nilesh Panchal <nilesh@ptotem.com>
2014-05-29 20:02:28 +0530

    model change

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,:confirmable,
          :recoverable, :rememberable, :trackable, :validatable, :omniauthable
 
+  validates :confirmation_token, :uniqueness => true, :allow_blank => true
+
   has_many :authentications, :dependent => :destroy
 
   has_one :demographic, dependent: :destroy
